3de9d99a9b647e18be920f47f8c0cf7d06310be3
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-07-24 15:41:49 +0000

    🚚 (assets): move GIF images from static folder to src/lib/img folder for better organization 🎨 (routes): update image paths in +page.svelte to use imported images for better resource management and loading efficiency 🎨 (routes): improve button styling and structure in +page.svelte for better user experience and consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,47 @@
 <script lang="ts">
 	import Navbar from '@/components/blocks/navbar.svelte';
+	import leftGif from '$lib/img/b3151c0a6afdefc2137c73255408a557-ezgif.com-crop.gif';
+	import rightGif from '$lib/img/111abeed6207fb60b16fc333a023d217-ezgif.com-crop.gif';
+	import { Play } from 'lucide-svelte';
 </script>
 
 <Navbar />
 
-<div class="container mx-auto mt-20 px-4 py-8 relative">  <h1 class="mb-4 text-center text-6xl font-bold text-gray-500">Deep Financing Tokens</h1>
+<div class="container relative mx-auto mt-20 px-4 py-8">
+	<h1 class="mb-4 text-center text-6xl font-bold text-gray-500">Deep Financing Tokens</h1>
 
 	<div class="mb-4">
-	  <p class="text-center text-6xl text-gray-500">A Fintech Innovation</p>
+		<p class="text-center text-6xl text-gray-500">A Fintech Innovation</p>
 	</div>
-  
+
 	<div class="flex justify-center">
-	  <div class="w-1/2 mr-4">
-		<img src="/b3151c0a6afdefc2137c73255408a557-ezgif.com-crop.gif" alt="GIF 1" class="w-full">
-	  </div>
-	  <p class="text-lg text-gray-500 max-w-2xl text-center">
-		Transforming Supply Chain Finance with Programmable Digital Assets. Bridging the Liquidity Gap in Global Supply Chains.
-	  </p>
-	  <div class="w-1/2 ml-4">
-		<img src="/111abeed6207fb60b16fc333a023d217-ezgif.com-crop.gif" alt="GIF 2" class="w-full">
-	  </div>
+		<div class="mr-4 w-1/2">
+			<img src={leftGif} alt="GIF 1" class="w-full" />
+		</div>
+		<p class="max-w-2xl text-center text-lg text-gray-500">
+			Transforming Supply Chain Finance with Programmable Digital Assets. Bridging the Liquidity Gap
+			in Global Supply Chains.
+		</p>
+		<div class="ml-4 w-1/2">
+			<img src={rightGif} alt="GIF 2" class="w-full" />
+		</div>
 	</div>
-  
-	<div class="absolute transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center mt-8" style="top: 250px; left: 50%;">  <button
-		type="button"
-		class="flex items-center space-x-2 rounded-lg bg-[#05A987] px-4 py-2 text-center text-sm font-medium text-white hover:bg-[#05A987] focus:outline-none focus:ring-4 focus:ring-[#05A987] dark:bg-[#05A987] dark:hover:bg-[#05A987] dark:focus:ring-[#05A987]"
-	  >
-		Book a Demo <span class="pl-2"></span>
-	  </button>
-	  <button
-      type="button"
-      class="flex items-center space-x-2 rounded-lg bg-white px-4 py-2 justify-content: center; align-items: center; gap: 10px; border-radius: 6px; 
-      color: var(--Foundation-Text-text-500, #292929);
-      font-family: Lexend;
-      font-size: 16px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 140%;"
-    >
-    How it works <span class="pl-2"><img src="/Frame 1973341766.png" alt="How it works icon" class="w-4 h-4 mr-2" /></span>
-    </button>
+
+	<div
+		class="absolute mt-8 flex -translate-x-1/2 -translate-y-1/2 transform items-center justify-center"
+		style="top: 250px; left: 50%;"
+	>
+		<button
+			type="button"
+			class="flex items-center space-x-2 rounded-lg bg-[#05A987] px-4 py-2 text-center text-sm font-medium text-white hover:bg-[#05A987] focus:outline-none focus:ring-4 focus:ring-[#05A987] dark:bg-[#05A987] dark:hover:bg-[#05A987] dark:focus:ring-[#05A987]"
+		>
+			Book a Demo
+		</button>
+
+		<button class="mx-3 flex items-center justify-center space-x-2 rounded-lg bg-white px-4 py-2">
+			How it works <span class="ml-2 rounded-full bg-[#05A987] p-1"
+				><Play class="fill-white" size={12} /></span
+			></button
+		>
 	</div>
-  </div>
-	
+</div>
