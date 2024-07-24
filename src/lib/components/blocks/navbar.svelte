f805351a9a78492996ebab59fb55ead3e5f11661
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-07-24 08:53:42 +0100

    ✨: Update navbar style and functionality
    Added lucide-svelte icons for menu and download button to enhance UI
    
    Changes:
    - Updated navbar styles for better readability
    - Added lucide-svelte icons for menu and download button
    - Made navbar improved user experience

--- src/lib/components/blocks/navbar.svelte
+++ src/lib/components/blocks/navbar.svelte
@@ -1,4 +1,11 @@
-<nav class="border-gray-200 bg-white dark:bg-gray-900">
+<script>
+	import { Download, Menu } from 'lucide-svelte';
+	let navOpen = false;
+</script>
+
+<nav
+	class="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900"
+>
 	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
 		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
 			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
@@ -9,37 +16,25 @@
 		<div class="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
 			<button
 				type="button"
-				class="rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>Get started</button
+				class="rounded-lg bg-[#05A987] px-4 py-2 text-center text-sm font-medium text-white hover:bg-[#05A987] focus:outline-none focus:ring-4 focus:ring-[#05A987] dark:bg-[#05A987] dark:hover:bg-[#05A987] dark:focus:ring-[#05A987]"
+				>Get the App <span><Download /></span></button
 			>
 			<button
-				data-collapse-toggle="navbar-cta"
+				data-collapse-toggle="navbar-sticky"
 				type="button"
 				class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-				aria-controls="navbar-cta"
+				aria-controls="navbar-sticky"
 				aria-expanded="false"
+				on:click={() => (navOpen = !navOpen)}
 			>
+				<Menu />
 				<span class="sr-only">Open main menu</span>
-				<svg
-					class="h-5 w-5"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 17 14"
-				>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M1 1h15M1 7h15M1 13h15"
-					/>
-				</svg>
 			</button>
 		</div>
 		<div
-			class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
-			id="navbar-cta"
+			class:hidden={!navOpen}
+			class=" w-full items-center justify-between md:order-1 md:flex md:w-auto"
+			id="navbar-sticky"
 		>
 			<ul
 				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse"
@@ -47,28 +42,28 @@
 				<li>
 					<a
 						href="#"
-						class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+						class="block rounded bg-blue-700 px-3 py-2 text-[#05A987] md:bg-transparent md:p-0 md:text-[#05A987] md:dark:text-[#05A987]"
 						aria-current="page">Home</a
 					>
 				</li>
 				<li>
 					<a
 						href="#"
-						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-[#05A987] md:dark:hover:bg-transparent md:dark:hover:text-[#05A987]"
 						>About</a
 					>
 				</li>
 				<li>
 					<a
 						href="#"
-						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-[#05A987] md:dark:hover:bg-transparent md:dark:hover:text-[#05A987]"
 						>Services</a
 					>
 				</li>
 				<li>
 					<a
 						href="#"
-						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-[#05A987] md:dark:hover:bg-transparent md:dark:hover:text-[#05A987]"
 						>Contact</a
 					>
 				</li>
