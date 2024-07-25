6dd02670a4d8d831c01ba8b0c4b490bcfdf24fba
Raj9101 <21051237@kiit.ac.in>
2024-07-25 21:40:20 +0530

    updated DFTFeaturesCont & Footer

---
+++ src/lib/img/checkbox.svelte
@@ -0,0 +1,15 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="fit" height="fit" viewBox="0 0 27 27" fill="none">
+    <g clip-path="url(#clip0_1_937)">
+      <path d="M4.45862 6.62484L6.08362 8.24984L8.79195 5.5415" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4.45862 13.1248L6.08362 14.7498L8.79195 12.0415" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M4.45862 19.6248L6.08362 21.2498L8.79195 18.5415" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12.5836 7.1665H22.3336" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12.5836 13.6665H22.3336" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M12.5836 20.1665H22.3336" stroke="#1D6AC4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    <defs>
+      <clipPath id="clip0_1_937">
+        <rect width="26" height="26" fill="white" transform="translate(0.666992 0.666504)"/>
+      </clipPath>
+    </defs>
+  </svg>
---
+++ src/lib/img/line1.svelte
@@ -0,0 +1,9 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="fit" height="fit" viewBox="0 0 27 7" fill="none">
+    <path d="M24 6.5C25.6569 6.5 27 5.15685 27 3.5C27 1.84315 25.6569 0.5 24 0.5V6.5ZM0.5 6.5H24V0.5H0.5V6.5Z" fill="url(#paint0_linear_1_896)"/>
+    <defs>
+      <linearGradient id="paint0_linear_1_896" x1="0.5" y1="4" x2="30.5" y2="4" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#05A987" stop-opacity="0"/>
+        <stop offset="1" stop-color="#05A987"/>
+      </linearGradient>
+    </defs>
+  </svg>
